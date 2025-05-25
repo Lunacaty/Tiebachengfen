3d87efe67a2692f4c95e3c6f32f89e5dcb4d4c75
Lunacaty <erocats@qq.com>
2025-05-25 18:43:43 +0800

    提速

--- src/main/java/com/tieba/Main.java
+++ src/main/java/com/tieba/Main.java
@@ -52,6 +52,7 @@ public class Main {
             
             ChromeOptions options = new ChromeOptions();
             
+            options.setPageLoadStrategy(org.openqa.selenium.PageLoadStrategy.NONE);
             options.addArguments("--disable-javascript");
             options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36...");
             Map<String, Element> map = main.getPages(page, forumName);
